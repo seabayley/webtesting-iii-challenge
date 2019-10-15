ac4fe01c4c8cbca2beaaf0f5c1b4003e2d788d9d
Christopher Bailey <seabayley@outlook.com>
2019-10-15 14:14:29 -0500

    Initial commit

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard'
+
+describe('<Dashboard />', () => {
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Dashboard />).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
